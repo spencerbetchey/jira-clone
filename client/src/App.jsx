42bebe1c2a3481c96b6acefad89aea19a43757fd
Spencer <spencerbetchey@gmail.com>
2026-06-24 11:47:41 -0500

    Set up React Router with placeholder pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Auth/Login'
+import Dashboard from './pages/Dashboard/Dashboard'
+import Projects from './pages/Projects/Projects'
+import Tickets from './pages/Tickets/Tickets'
+
 function App() {
   return (
-    <div>
-      <h1>Jira Clone</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/tickets" element={<Tickets />} />
+    </Routes>
   )
 }
 
---
+++ client/src/pages/Tickets/Tickets.jsx
@@ -0,0 +1,9 @@
+function Tickets() {
+  return (
+    <div>
+      <h1>Tickets</h1>
+    </div>
+  )
+}
+
+export default Tickets
