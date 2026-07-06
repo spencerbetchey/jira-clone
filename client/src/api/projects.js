0680f923f640656d57ca64abb45f173f3d2baaea
Spencer <spencerbetchey@gmail.com>
2026-07-06 12:18:49 -0500

    Created API helper file

---
+++ client/src/api/projects.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+
+const API_URL = 'http://localhost:5000/api/projects'
+
+const getAuthHeader = () => {
+  const token = localStorage.getItem('token')
+  return { Authorization: `Bearer ${token}` }
+}
+
+export const fetchProjects = async () => {
+  const res = await axios.get(API_URL, { headers: getAuthHeader() })
+  return res.data.projects
+}
+
+export const fetchProject = async (id) => {
+  const res = await axios.get(`${API_URL}/${id}`, { headers: getAuthHeader() })
+  return res.data.project
+}
+
+export const createProject = async (projectData) => {
+  const res = await axios.post(API_URL, projectData, { headers: getAuthHeader() })
+  return res.data.project
+}
+
+export const updateProject = async (id, projectData) => {
+  const res = await axios.put(`${API_URL}/${id}`, projectData, { headers: getAuthHeader() })
+  return res.data
+}
+
+export const deleteProject = async (id) => {
+  const res = await axios.delete(`${API_URL}/${id}`, { headers: getAuthHeader() })
+  return res.data
+}
